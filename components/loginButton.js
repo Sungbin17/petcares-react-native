63a539adb01f9ba718f5c7aa051eba7ce144c877
concip <qkrgowls97@naver.com>
2020-05-08 13:34:27 +0900

    home

---
+++ components/loginButton.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import {
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+} from 'react-native';
+
+export default class LoginButton extends Component{
+  static defaultProps = {
+    title: 'untitled',
+    buttonColor: '#000',
+    titleColor: 'black',
+    onPress: () => null,
+  }
+
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return (
+      <TouchableOpacity style={[
+        styles.button,
+        {backgroundColor: this.props.buttonColor}
+      ]}
+      onPress={this.props.onPress}>
+        <Text style={[
+          styles.title,
+          {color: this.props.titleColor}
+        ]}>{this.props.title}</Text>
+      </TouchableOpacity>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  button: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginRight: 20,
+    borderRadius: 5,
+  },
+  title: {
+    fontSize: 15,
+  },
+});
--- screens/Home/HomeScreen.js
+++ screens/Home/HomeScreen.js
@@ -1,34 +1,97 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Image, TouchableOpacity } from "react-native";
+import LoginButton from '../../components/loginButton';
 
 class HomeScreen extends React.Component {
 
   static navigationOptions = {
-    headerTitle: "펫돌보미"
-};
-
+    headerTitle: (
+        <View>
+            <Text style={{ alignItems: 'center', color: 'black', }}>
+                펫 돌보미
+            </Text>
+        </View>
+    ),
+    headerLeft: (
+        <Image
+            style={{marginLeft: 20, width: 30, height: 30, resizeMode: 'contain'}}
+            source={require('../images/logo.jpg')} />
+    ),
+    headerRight: (
+        <LoginButton
+            style={{paddingRight: 30}}
+            buttonColor={'#fdeaab'}
+            title={'login'}
+            onPress={()=> alert('로그인창으로 넘어가야하는데,,,')} />
+    ),
+    headerTitleStyle: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    headerStyle: {
+        flexDirection: 'row',
+        backgroundColor: '#fdeaab',
+    }
+  };
   componentDidMount() {
+  }
 
+  _pushLogin(){
+    this.props.navigator.push({
+      screen: 'yuddomack.login', // unique ID registered with Navigation.registerScreen
+      title: 'LOGIN', // navigation bar title of the pushed screen (optional)
+      passProps: {}, // Object that will be passed as props to the pushed screen (optional)
+    });
   }
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text>
-          여기다 이렇게 
-          내가 이거 코드 올려주면 작업해서 코드 합치면 될듯
-        </Text>
-
-
-      </View>
+        <View>
+            <View style={styles.slide}>
+                <Text style={{ padding: 20, align: 'center' }}>
+                    반갑습니다!
+                </Text>
+            </View>
+            <View style={styles.container}>
+                <Text>
+                미용, 호텔, 병원
+                </Text>
+            </View>
+            <View style={styles.footer}>
+                <Text style={{color: '#a0855b'}}>
+                Back To Top
+                </Text>
+                <Text></Text>
+                <Text style={{color: '#a0855b'}}>
+                Language                 privacy                  Terms
+                </Text>
+                <Text></Text>
+                <Text style={{fontSize: 20}}>
+                © Copyright 2019, All Rights Reserved
+                </Text>
+            </View>
+        </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, padding: 24, backgroundColor: 'white'
-  }
+    flex: 1,
+    padding: 24,
+    paddingBottom: 40,
+    backgroundColor: 'white',
+    alignItems: 'center'
+  },
+  slide: {
+    backgroundColor: '#8f8f8f',
+    alignItems: 'center'
+  },
+  footer: {
+    backgroundColor: '#fdeaab',
+    padding: 5,
+    alignItems: 'center'
+  },
 });
 
 export default HomeScreen;
